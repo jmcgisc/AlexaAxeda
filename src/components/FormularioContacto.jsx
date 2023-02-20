fb18be342679e732765ebfa75787afacef26ad38
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-02-20 01:53:57 -0600

    formulario de contacto V.1.0

--- src/components/FormularioContacto.jsx
+++ src/components/FormularioContacto.jsx
@@ -4,11 +4,10 @@ import {ImLocation} from "react-icons/im"
 
 const FormularioContacto = () => {
     return ( 
-        <div className ="antialiased bg-gray-100">
+        <div className ="antialiased bg-gray-100 overflow-hidden">
 
-            <div className="flex flex-col w-full justify-center items-center">
-                <div className ="flex flex-col md:flex-row md:space-x-6 md:space-y-0 space-y-6 bg-cyan-700   p-8 
-                rounded-xl shadow-lg text-white">  
+            <div className="flex flex-col w-full justify-center items-center overflow-hidden">
+                <div className ="flex flex-col md:flex-row md:space-x-6 md:space-y-0 space-y-6 bg-cyan-700 p-8 rounded-xl shadow-lg text-white overflow-hidden">  
                         
                         <div className ="flex flex-col space-y-8 justify-berween">
                             <div>    
@@ -24,14 +23,14 @@ const FormularioContacto = () => {
                                 <span> asesor.alexadelgado@gmail.com </span>  
                             </div> 
                             <div className="inline-flex space-x-2 items-center">
-                                <ImLocation className="text-teal-300 text-xl"/>
+                                <ImLocation className="text-teal-300 text-xl"/>º
                                 <span> Cd. México, Merida, Cancún </span>  
                             </div>  
 
                             </div>
                         <div className="relative"> 
-                        {/* <div className="absolute z-0 w-40 h-40 bg-teal-400 rounded-full -right-28 -right-top-28"></div>
-                        <div className="absolute z-0 w-40 h-40 bg-teal-400 rounded-full -left-28 -right-bottom-10"></div> */}
+                         <div className="absolute z-0 w-40 h-40 bg-teal-400 rounded-full -right-28 -right-top-28"></div>
+                        <div className="absolute  w-40 h-40 bg-teal-400 rounded-full -left-16 -bottom-16"></div> 
  
                         <div className="relative z-10 bg-white rounded-xl shadow-lg hover:shadow-indigo-500/40 p-8 text-gray-600 md:w-90 " >
 
