8a40f50cb2904b9e3f7995da07a8fbc85d71761d
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-02-25 16:08:47 -0600

    menu con enlace

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -61,8 +61,10 @@ const Headers = () => {
                <div className="py-1">
                   <Menu.Item>
                   {({ active }) => (
-                     <NavLink  className ="bg-gray-100 text-gray-900' : 'text-gray-700',
-                     'block px-4 py-2 text-sm font-mono text-center"
+                     <NavLink  className ={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm font-mono text-center'
+                        )}
  
                            to="/rosavento" 
                         >
