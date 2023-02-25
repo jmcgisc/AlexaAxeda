6025e9859c8c20ade3bf546b7197a522e51e6f44
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-02-24 23:20:14 -0600

    folder dist

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
             <Route path = '/components/footer/Terminos'       element ={ <Terminos/> } />
             <Route path = '/components/footer/Privacidad'     element ={ <Privacidad/> } /> */}
                 
-          </Routes> 
+          </Routes>  
  
         <Footer/>     
               {/* 
