83898c5832b5b6266beea77862eec2356e946cbc
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-03-06 12:49:18 -0600

    brochure desarrollos

--- src/components/Desarrollos/Rosavento.jsx
+++ src/components/Desarrollos/Rosavento.jsx
@@ -1,51 +1,31 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 import Banner from "../Aux/Banner"
+import pdf from '../../../public/ROSAVENTO CANCÚN_Brochure.pdf'
+import pdf from '../../../public/Brochure_Rosavento_Merida.pdf'
 
 const Rosavento = () => { 
     return (
         <> 
 
-<div className="bg-no-repeat bg-cover bg-center bg-fixed w-full h-full bg-auto
-        bg-[url('https://st.depositphotos.com/1491329/2197/i/950/depositphotos_21976063-stock-photo-ik-kil-cenote-chichen-itza.jpg')]">
-            INVIERTE DESDE $2,571,426 MXN
- 
+        <div className="bg-no-repeat bg-cover bg-center bg-fixed bg-auto opacity-70
+                bg-[url('https://st.depositphotos.com/1491329/2197/i/950/depositphotos_21976063-stock-photo-ik-kil-cenote-chichen-itza.jpg')]">
 
- Este desarrollo está inspirado en la arquitectura de las antiguas ciudades mayas y busca celebrar la naturaleza exterior, escondiendo las edificaciones entre la selva para ofrecerte un ambiente privado, orgánico y tranquilo. Aquí, vivirás rodeado de vegetación endémica que será el escenario perfecto para tu casa, ese lugar especial para ti y tu familia.
- 
-  
- 
- CTA-Kaybe-Lotes-2    INVIERTE DESDE $2,571,426 MXN
- 
+           <div className="bg-no-repeat bg-cover bg-center bg-fixed w-full h-full py-2
+                                                                bg-[url('/public/fondo2.jpg')] text-white ">
 
- Este desarrollo está inspirado en la arquitectura de las antiguas ciudades mayas y busca celebrar la naturaleza exterior, escondiendo las edificaciones entre la selva para ofrecerte un ambiente privado, orgánico y tranquilo. Aquí, vivirás rodeado de vegetación endémica que será el escenario perfecto para tu casa, ese lugar especial para ti y tu familia.
- 
-  
- 
- CTA-Kaybe-Lotes-2    INVIERTE DESDE $2,571,426 MXN
- 
-
- Este desarrollo está inspirado en la arquitectura de las antiguas ciudades mayas y busca celebrar la naturaleza exterior, escondiendo las edificaciones entre la selva para ofrecerte un ambiente privado, orgánico y tranquilo. Aquí, vivirás rodeado de vegetación endémica que será el escenario perfecto para tu casa, ese lugar especial para ti y tu familia.
- 
-  
- 
- CTA-Kaybe-Lotes-2    INVIERTE DESDE $2,571,426 MXN
- 
-
- Este desarrollo está inspirado en la arquitectura de las antiguas ciudades mayas y busca celebrar la naturaleza exterior, escondiendo las edificaciones entre la selva para ofrecerte un ambiente privado, orgánico y tranquilo. Aquí, vivirás rodeado de vegetación endémica que será el escenario perfecto para tu casa, ese lugar especial para ti y tu familia.
- 
-  
- 
- CTA-Kaybe-Lotes-2    INVIERTE DESDE $2,571,426 MXN
- 
-
- Este desarrollo está inspirado en la arquitectura de las antiguas ciudades mayas y busca celebrar la naturaleza exterior, escondiendo las edificaciones entre la selva para ofrecerte un ambiente privado, orgánico y tranquilo. Aquí, vivirás rodeado de vegetación endémica que será el escenario perfecto para tu casa, ese lugar especial para ti y tu familia.
- 
-  
- 
- CTA-Kaybe-Lotes-2
-        </div>
-//paralax
+              <div class="flex flex-wrap grid grid-cols-6 gap-4"> 
+            <div class="col-start-1 col-end-3"></div> 
+            <div class="col-start-2 col-span-4 ">
+                <h2 className="text-3xl text-center ">Descubre las  </h2>
+                <h1 className="text-5xl font-bold py-3 tracking-wide text-center">OPORTUNIDADES DE ROSAVENTO</h1>
+            </div>
+            <div class="col-start-1 col-end-7 py-3 text-center ">
+                <p>Elige e invierte en tu próximo patrimonio en las zonas con mejor plusvalía de Mérida y  Cancún. </p>
+            </div>
+            </div>
+            </div>
+        </div> 
         <div className="h4/5">
 
         </div>
@@ -55,22 +35,19 @@ const Rosavento = () => {
         </div>
         
         <div className="bg-no-repeat bg-cover bg-center bg-fixed w-full h-full
-        bg-[url('/public/fondo2.jpg')] text-white hover:bg-gradient-to-r">
-              <div class="flex flex-wrap grid grid-cols-6 gap-4"> 
-            <div class="col-start-1 col-end-3"></div> 
-            <div class="col-start-2 col-span-4 ">
-                <h2 className="text-xl ">Descubre las  </h2>
-                <h1 className="text-2xl underline italic font-bold tracking-wide">oportunidades de Rosavento</h1>
-            </div>
-            <div class="col-start-2 col-end-7 py-3 px-8 ">
-                <p>Elige e invierte en tu próximo patrimonio en las zonas con mejor plusvalía de Mérida y  Cancún. </p>
+            bg-[url('/public/fondo2.jpg')] text-white hover:bg-gradient-to-r">
+                <div class="flex flex-wrap grid grid-cols-6 gap-4"> 
+                <div class="col-start-1 col-end-3"></div> 
+                <div class="col-start-2 col-span-4 ">
+                    <h2 className="text-xl ">Descubre las  </h2>
+                    <h1 className="text-2xl underline italic font-bold tracking-wide">oportunidades de Rosavento</h1>
+                </div>
+                <div class="col-start-2 col-end-7 py-3 px-8 ">
+                    <p>Elige e invierte en tu próximo patrimonio en las zonas con mejor plusvalía de Mérida y  Cancún. </p>
+                </div>
             </div>
         </div>
-        </div>
 
- 
-
-       
 
         <div class="flex flex-wrap grid grid-cols-6 gap-4"> 
             <div class="col-start-1 col-end-3"></div> 
@@ -182,7 +159,23 @@ const Rosavento = () => {
                 </p>
             </div>
         </div>
-        
+
+        <div> 
+            <button className={`btn`}>
+                <a href={pdf} target="_blank" rel="noopener noreferrer" download="ROSAVENTO CANCÚN_Brochure.pdf"> 
+                    Brochure Rosavento Cancún 
+                </a>
+            </button> 
+        </div>
+
+        <div> 
+            <button className={`btn`}>
+                <a href={pdf} target="_blank" rel="noopener noreferrer" download="Brochure_Rosavento_Merida.pdf"> 
+                    Brochure Rosavento Mérida 
+                </a>
+            </button> 
+        </div>
+
         </>
     )
 }
