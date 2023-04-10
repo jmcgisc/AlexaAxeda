6befe0297afef54bba052fa1b12c81ce894d7fa6
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-04-09 20:06:44 -0600

    master plan

--- src/components/Desarrollos/Rosavento.jsx
+++ src/components/Desarrollos/Rosavento.jsx
@@ -205,12 +205,6 @@ const Rosavento = () => {
 
             <LayoutMap/>    
 
-        {/*Master Plan*/}
-        <hr
-            className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100"/>
-
-
-
         {/*By Grupo Axeda*/}
         <hr
             className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100"/>
