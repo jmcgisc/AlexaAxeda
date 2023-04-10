796cb361d6431bb81652ac89fbfeb58aa8743f03
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-04-10 00:07:22 -0600

    politica de privacidad

--- src/components/Aux/Privacidad.jsx
+++ src/components/Aux/Privacidad.jsx
@@ -2,45 +2,119 @@ import React from 'react'
 
 const Privacidad = () => {
   return (
-    <div>
-    hola
-        hola
-        hola
-        hola
-        hola
-        hola
-        hola
-    hola
-        hola
-        hola
-        hola
-        hola
-        hola
-        hola
-    hola
-        hola
-        hola
-        hola
-        hola
-        hola
-        hola
-    hola
-        hola
-        hola
-        hola
-        hola
-        hola
-        hola
-    hola
-        hola
-        hola
-        hola
-        hola
-        hola
-        hola
-      
-    </div>
-  )
+        <>
+        <div>
+            <h1 class="text-axeda text-center font-title text-2xl lg:text-5xl mt-5"><span>Axeda </span>- Política de privacidad-</h1>
+        </div> 
+
+        {/*Texto politica privacidad*/}
+        <div className="grid grid-cols-1 bg-[url('/public/homeBack2.png')] rounded">
+            <div class="text-gray-600 text-xl font-body text-justify px-12 py-2"> 
+                <h2 className="text-gray-600 text-xl font-body text-justify py-2">
+                    RESPONSABLE DE LOS DATOS PERSONALES
+                </h2>
+            </div >
+        </div >
+
+        <p className="mb-3 mt-3 px-3 text-base">
+            Con fundamento en la Ley Federal de Protección de Datos Personales en Posesión de Particulares (en adelante la Ley) le informamos que la persona moral DESARROLLOS INMOBILIARIOS AXEDA S.A DE C.V. (en adelante “La Empresa”), con domicilio en la calle 21 número 125A de la Colonia Campestre, de la ciudad y municipio de Mérida, Yucatán C.P. 97120, es una empresa comprometida en proteger su privacidad, y es el responsable de su Tratamiento (término que se define más adelante) cuando sean recabados directamente por ella o a través de algún medio electrónico de su pertenencia, y por tanto le informamos lo siguiente en cuanto a:
+        </p>
+
+        <div className="grid grid-cols-1 bg-[url('/public/homeBack2.png')] rounded">
+            <div class="text-gray-600 text-xl font-body text-justify px-12 py-2"> 
+                <h2 className="text-gray-600 text-xl font-body text-justify py-2">
+                    TRATAMIENTO DE DATOS PERSONALES
+                </h2>
+            </div>
+        </div>
+        
+        <p className="mb-3 mt-3 px-3 text-base">
+            La Empresa podrá solicitar y/o recabar directamente, los Datos Personales de los clientes para su uso comercial, divulgación y/o almacenamiento por cualquier medio para los fines abajo señalados. La Empresa y/o cualquier tercero que llegue a intervenir en cualquier fase del Tratamiento de Datos Personales guardarán confidencialidad respecto de los mismos cuando tengan dicho carácter, conforme a las disposiciones legales aplicables en México.
+            El ingreso y/o registro de Datos Personales implica el consentimiento pleno y sin reservas de los clientes para el Tratamiento de sus Datos Personales de acuerdo con el presente Aviso de Privacidad. En el caso de que se requieran Datos Financieros (como más adelante se define) será necesario que el usuario otorgue su consentimiento expreso antes de proporcionar los mismos.
+            Todos los datos proporcionados serán privados y de ninguna manera estarán disponibles al público.
+        </p>   
+
+        <div className="grid grid-cols-1 bg-[url('/public/homeBack2.png')] rounded">
+            <div class="text-gray-600 text-xl font-body text-justify px-12 py-2"> 
+                <h2 className="text-gray-600 text-xl font-body text-justify py-2">
+                    FINALIDAD Y TRATAMIENTO DE LA INFORMACIÓN PERSONAL
+                </h2>
+            </div>
+        </div>
+
+
+        <p className="mb-3 mt-3 px-3 text-base">
+            Su información personal será utilizada para los siguientes fines: Confirmar su identidad y verificar la información proporcionada; Recibir los servicios o productos contratados; Elaborar contratos y atender sus necesidades legales; cumplir con requisitos legales o normativos que le sean aplicables a los desarrollos; Hacerle llegar notificaciones respecto al producto contratado; Enviarle Publicidad Informativa de nuestros servicios y proyectos; Elaborar estudios de mercadotecnia, segmentación de mercado e información estadística; Actualizar nuestra Base de Datos de Clientes; Realizar encuestas sobre la calidad en el servicio; Demás usos necesarios para la correcta comercialización de los servicios y/o productos de La Empresa.
+
+            La información que le podrá será requerida será la siguiente: nombre completo, dirección (calle, número exterior, número interior, colonia, código postal, estado, delegación o municipio), teléfono, celular, fecha de nacimiento, estado civil, correo electrónico e información bancaria, como: el número de tarjeta de crédito o débito, cuenta, clabe Interbancaria, así como sucursal e Institución Financiera*.
+
+            *Datos solicitados para que los clientes paguen con instrumentos como la tarjeta de crédito, débito o cheques el producto de La Empresa.
+
+            El tratamiento de su información personal se realizará con base en los principios de licitud, calidad, consentimiento, información, finalidad, lealtad, proporcionalidad y responsabilidad consagrados en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares.
+        </p>   
+
+        <div className="grid grid-cols-1 bg-[url('/public/homeBack2.png')] rounded">
+            <div class="text-gray-600 text-xl font-body text-justify px-12 py-2"> 
+                <h2 className="text-gray-600 text-xl font-body text-justify py-2">
+                    LIMITACIÓN DE USO DE DATOS PERSONALES
+                </h2>
+            </div>
+        </div>
+
+        <p className="mb-3 mt-3 px-3 text-base">
+
+        La Empresa cuenta con medidas físicas, técnicas y administrativas de seguridad para la protección de Datos Personales proporcionados por los clientes.
+        Si usted desea dejar de recibir mensajes promocionales vía correo electrónico y/o de forma impresa y/o vía telefónica, La Empresa pone a disposición de los clientes el siguiente correo electrónico: clientes@axeda.mx
+        </p>   
+
+        <div className="grid grid-cols-1 bg-[url('/public/homeBack2.png')] rounded">
+            <div class="text-gray-600 text-xl font-body text-justify px-12 py-2"> 
+                <h2 className="text-gray-600 text-xl font-body text-justify py-2">
+                    LIMITACIÓN AL USO O DIVULGACIÓN DE INFORMACIÓN PERSONAL
+                </h2>
+            </div>
+        </div>
+
+        <p className="mb-3 mt-3 px-3 text-base">
+        Sus datos personales en ningún momento serán expuestos, vendidos, divulgados o transferidos con tercero sin su autorización previa, únicamente se les dará el uso señalado en las finalidades especificadas en el presente aviso de privacidad.
+
+        DEL EJERCICIO DE LOS DERECHOS DE ACCESO, RECTIFICACIÓN, CANCELACIÓN Y OPOSICIÓN.
+
+        Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, en su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de nuestro registros o base de datos cuando considere que la misma no está siendo utilizada conforme a los principios, deberes y obligaciones previstas en la normativa (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derecho ARCO.
+        El ejercicio de estos derechos deberá realizarse previa solicitud a La Empresa y contener al menos lo siguiente:
+        </p>  
+
+        <ul role="list" class="marker:text-axeda list-disc pl-5 space-y-3 text-slate-700 mb-3 mt-3">
+            <li>El nombre del titular, domicilio y correo electrónico para comunicarle la respuesta a su solicitud.</li>
+            <li>Los documentos que acrediten la identidad o, en su caso, la representación legal del titular.</li>
+            <li>La descripción clara y precisa de los Datos Personales respecto de los que se busca ejercer alguno de los derechos antes mencionados.</li>
+            <li>Cualquier otro elemento o documento que facilite la localización de los datos personales.</li>
+        </ul>
+         
+
+        <p className="mb-3 mt-3 px-3 text-base">
+        Para efecto de lo anterior La Empresa pone a disposición de los clientes el siguiente correo electrónico: clientes@axeda.mx.La Empresa dará respuesta a la solicitud de los clientes en un plazo no mayor a 5 días hábiles contados a partir de la fecha en que reciba la petición correspondiente, a través del correo electrónico que haya sido proporcionado para tal efecto. El ejercicio por parte de los clientes de los derechos antes mencionados deberá sujetarse a las leyes y reglamentos aplicables vigentes en México.
+
+        La negativa para el uso de sus datos personales sobre las finalidades secundarias, no podrá ser un motivo para que le neguemos los productos y/o servicios que solicita o contrate con nosotros.
+        </p>  
+
+        <div className="grid grid-cols-1 bg-[url('/public/homeBack2.png')] rounded">
+            <div class="text-gray-600 text-xl font-body text-justify px-12 py-2"> 
+                <h2 className="text-gray-600 text-xl font-body text-justify py-2">
+                    CAMBIOS A ESTE AVISO DE PRIVACIDAD
+                </h2>
+            </div>
+        </div>
+
+        <p className="mb-3 mt-3 px-3 text-base">
+            El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; por los cambios en nuestros servicios que ofrecemos; de nuestras prácticas de privacidad. Por lo que nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente Aviso de Privacidad, a través de nuestro sitio de Internet https://axeda.mx en la sección Avisos de Privacidad.
+
+            Nuestro sitio web https://axeda.mx garantiza que la información personal que usted envía cuenta con la seguridad necesaria. Los datos ingresados por usuario o en el caso de requerir una validación de los pedidos no serán entregados a terceros, salvo que deba ser revelada en cumplimiento a una orden judicial o requerimientos legales.
+            Enterado del presente aviso, el usuario manifiesta su conformidad que la empresa utilice sus datos personales para tal efecto, así como a recibir en lo subsecuente cualquier tipo de información que se encuentre relacionada con el servicio o desarrollo adquirido.
+        </p> 
+
+    </>
+    )
 }
 
 export default Privacidad
