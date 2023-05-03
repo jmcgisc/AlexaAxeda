e9053e3ad801c1a0363496b400d2957feb1c730d
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-05-03 16:12:26 -0600

    bio

--- src/components/Desarrollos/Rosenda.jsx
+++ src/components/Desarrollos/Rosenda.jsx
@@ -173,9 +173,8 @@ const Rosenda = () => {
                                     height='100%' 
                         />
 
-                    </div>
-            {/* <img class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96" src="https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"alt=""/> */}
-
+                    </div> 
+                    
                 <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 "> 
 
                     <p class="block mt-4 text-2xl font-semibold text-axeda hover:underline md:text-3xl">
