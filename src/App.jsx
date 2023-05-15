0cd15cec9c596c3f2c30cc3613d6314446465d34
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-05-15 16:57:06 -0600

    optimizacion imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); 
-const { pathname, search } = useLocation();
+// const analytics = getAnalytics(app); 
+// const { pathname, search } = useLocation();
 
 function App() { 
    
