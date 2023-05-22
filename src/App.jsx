ceda24574c7431d6277d2f300b39bd827dc524ff
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-05-21 23:46:02 -0600

    imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -70,6 +70,7 @@ function App() {
               statusMessage = "Desarrollos Axeda"
               darkMode="enable"
               chatMessage ="¡Hola! ¿En que proyecto estas interesado? 🤝 "   
+              avatar="public/AlexaDelgado.webp"
         />
  
         <Footer/>     
