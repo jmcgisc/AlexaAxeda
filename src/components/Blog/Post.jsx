0d8634ff11f037ed64406f17a3934a629d46daa9
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-06-14 20:32:12 -0600

    fuera promo

---
+++ src/components/Blog/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Blog from '../../../_posts/article.md' 
+
+
+const Post = () => {
+  return (
+    <div>
+
+
+      
+    <Blog />
+      
+    </div>
+  )
+}
+
+export default Post
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'  
+import Markdown from '@pity/vite-plugin-react-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Markdown({
+      // wrapperComponentName: 'ReactMarkdown',
+      // wrapperComponentPath: './src/components/page',
+      // if you want use components in md file, please add it in this
+      // [ComponentName]: `componentPath`
+      // // ?‍?: the `ComponentName` must be `CamelCase`
+      // importComponentsPath: {
+      //   ReactTest: './src/components/pages/mdtest',
+      // },
+      // markdownItUses: [
+      //   prism,
+      // ],
+    }),
+  ],
 })
- 
