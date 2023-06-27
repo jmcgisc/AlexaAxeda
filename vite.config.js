32ba97e152258e655fa3d570df8b02f17b116b52
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-06-26 21:37:50 -0600

    optimizacion

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'  
 import Markdown from '@pity/vite-plugin-react-markdown'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    viteCompression(),
     Markdown({
       // wrapperComponentName: 'ReactMarkdown',
       // wrapperComponentPath: './src/components/page',
