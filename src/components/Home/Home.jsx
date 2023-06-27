5ea45ac1121590594b367548505afed3d4137f00
Manu Carreiro <43585302+jmcgisc@users.noreply.github.com>
2023-06-26 22:52:33 -0600

    meta description

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,15 @@ import {RiCheckboxBlankCircleFill, RiPlayFill, RiStarFill}
                     from "react-icons/ri";
 import Popup        from 'reactjs-popup'; 
 import Desarrollos  from "./Desarrollos";
-import Slider       from '../Aux/Slider';
-import VideoGobernador    from "../Aux/VideoGobernador";
+import Slider       from '../Aux/Slider'; 
+import YoutubeEmbed from "../Aux/YoutubeEmbed"
+
 import Reviews      from "./Reviews";
 import Menciones    from "./Menciones";
 import FormularioContacto from "../Aux/FormularioContacto";
 import 'reactjs-popup/dist/index.css';
+import "../Aux/style.css";
+
 import { getAnalytics, logEvent, setUserProperties } from "firebase/analytics"; 
 
 import FormularioContactoPopup from '../Aux/FormularioContactoPopup';
@@ -142,8 +145,38 @@ export const Home = () => {
             <Slider/>
             
             {/*Gobernador Kuri*/}
-            <VideoGobernador/>
+            <div className="bg-BackgroundGray lg:py-24 flex flex-col items-center justify-center gap-8 mt-20 xl:mt-0 overflow-x-auto">
             
+             {/*/ Desarrollos Axeda */} 
+            <div className="container px-6 çlg:py-8 mx-auto">
+                <h1 className="text-3xl font-title text-center text-axeda capitalize lg:text-4xl"> 
+                  Evento de "Supervisión de obra de " <strong className="underline decoration-axeda/[.33]"> ROSAVENTO Ciudad Maya.</strong>
+                </h1>
+  
+                <div className="lg:mt-14 lg:-mx-6 lg:flex lg:items-center">
+                    <div className="object-cover  w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96">
+                          <div className="App">
+                              <YoutubeEmbed embedId="FEHDi1T5dFM" />
+                          </div>
+                    </div> 
+                            <div className="lg:w-1/2 lg:mt-0 lg:mx-6 "> 
+                                <p className="block text-2xl font-semibold text-axeda hover:underline md:text-3xl">
+                                    Julián Zacarías Curi
+                                </p>
+
+                                <p className="mt-3 text-base lg:text-xl lg:tracking-widest text-gray-700 font-body text-justify">
+                                        Presidente Municipal de Progreso  
+                                </p>
+
+                                <p className="mt-3 text-base  lg:text-xl lg:tracking-widest text-gray-700 font-body text-justify">
+                                    <strong> El presidente municipal de Progreso, Julían Zacarías Curi, en el acto de Supervisión de la Obra
+                                        de Rosavento, Ciudad Maya, felicita al grupo Axeda por la apuesta y progreso de esta infraestructura. </strong>
+                                </p>
+                            </div>
+                        </div>
+                </div> 
+                </div>
+                    
             {/*Opioniones*/}
             <Reviews/>
             
