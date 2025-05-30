d478bd073c0e83a90613df768fb67663ca910320
Jose Carreiro <jmcgisc@gmail.com>
2025-05-29 23:58:14 -0600

    isla diamante

--- src/components/Home/Desarrollos.jsx
+++ src/components/Home/Desarrollos.jsx
@@ -1,5 +1,31 @@
-import React        from 'react';
 import { NavLink }  from "react-router-dom"; 
+
+const desarrollos = [
+  {
+    nombre: "Azores – Colección Atlántica",
+    descripcion: "Minimalismo elegante, inspiración oceánica. Azores evoca la pureza del Atlántico con arquitectura sobria y estética. Rodeado de serenidad, diseño consciente y lujo. Mensualidades: $8,160.00 MXN.",
+    imagen: "Anzores.jpeg",
+    to: "/rosavento"
+  },
+  {
+    nombre: "Madeira – Colección Natural",
+    descripcion: "Refugio entre lo verde y lo sofisticado. Inspirada en los paisajes de Portugal, Madeira ofrece diseño sobrio, vegetación y materiales nobles. Mensualidades: $3,400.00 MXN.",
+    imagen: "Madeira.jpeg",
+    to: "/rosenda"
+  },
+  {
+    nombre: "Boracay – Colección Tropical",
+    descripcion: "Sofisticación cálida, esencia insular. Ambientes amplios, luz natural y acabados cálidos crean un oasis de relajación. Mensualidades: $3,124.00 MXN.",
+    imagen: "Madeira.jpeg",
+    to: "/terravento"
+  },
+  {
+    nombre: "Santorini",
+    descripcion: "Donde la elegancia se encuentra con el confort. Inspirado en los paisajes del mar Egeo. Privada con vista al lago, lujo cotidiano y seguridad. Mensualidades: $6,260.89 MXN.",
+    imagen: "Santorini.jpeg",
+    to: "/indo"
+  }
+];
 
 const Desarrollos = () => {
     return (
@@ -9,7 +35,7 @@ const Desarrollos = () => {
                     Donde vivir e invertir coexisten
                 </h1>
                 <p className="text-xl text-gray-400 py-4 font-sans">
-                    Desarrollos residenciales en Cancún y Mérida. Departamentos con las mejores ubicaciones y más.
+                    Desarrollos residenciales en Cancún. Cuatro colecciones. Cuatro formas de vivir con distinción.
                 </p>
             </div>
             
@@ -17,18 +43,28 @@ const Desarrollos = () => {
             <div className="grid grid-cols-1 xl:grid-cols-2 gap-8">
                 <NavLink className ="text-center " to = "/rosavento" > 
                     <div className="flex flex-col gap-2 rounded-3xl py-2"> 
-                            <img src="Santorini.jpeg"  
+                            <img src="Isla_2.webp"  
                                  class="h-56 xl:h-[600px] w-full rounded-lg  shadow-xl dark:shadow-gray-800 transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-70  "  
                             // className="inline w-full h-56 xl:h-[600px] rounded-3xl transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-70 hover:shadow-2xl" 
                                     alt='Terrenos residenciales Isla Dimante'/>
-                                <h3 className="text-2xl font-bold text-gray-400 ">Santorini</h3> 
-                                <p>  Donde la elegancia se encuentra con el confort.
-                                    Inspirado en los paisajes blancos y azules de la joya del mar Egeo, esta privada redefine el lujo cotidiano en un ambiente íntimo, seguro y cuidadosamente diseñado con una increíble vista al lago. Mensualidades $6,260.89 MXN.
-                                </p>
+                                <h3 className="text-2xl font-bold text-gray-400 ">Isla Diamante</h3> 
+                                <p>
+                              
+                                    En Isla Diamante, cada privada es una expresión de estilo, lujo y carácter. Inspiradas en rincones icónicos del mundo, nuestras colecciones residenciales ofrecen experiencias de vida únicas, diseñadas para quienes valoran lo excepcional. Cuatro colecciones. Cuatro formas de vivir con distinción.                                    
+                                  </p>
                     </div>          
                 </NavLink>   
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+     <NavLink className ="text-center " to = "/indo" > 
+                        <div className="flex flex-col gap-4">
+                                <img src="Santorini.jpeg" className="w-full h-56 object-cover rounded-3xl transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl"
+                                    alt='Yucatan crecimiento económico'/>
+                    
+                                    <h3 className="text-2xl font-bold text-gray-400 ">Santorini - Colección Mediterranea</h3>
+                        </div>
+                    </NavLink>   
 
                     <NavLink className ="text-center " to = "/rosavento" > 
                         <div className="flex flex-col gap-4">
@@ -36,10 +72,7 @@ const Desarrollos = () => {
                                 <img  src="Anzores.jpeg" className="w-full h-56 object-cover rounded-3xl 
                                         transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl" 
                                         alt='Desarrollos en Ciudad Maya' />
-                                <p className="text-gray-500 font-chivo"> 
-                                    Minimalismo elegante, inspiración oceánica.
-                                    Azores evoca la pureza del Atlántico con una propuesta arquitectónica limpia, sobria y profundamente estética. Un estilo de vida refinado, rodeado de serenidad y diseño consciente, para quienes buscan una residencia con carácter. Menusualidades: $8,160.00 MXN
-                                </p>
+                       
                                     <h3 className="text-2xl font-bold text-gray-400 ">Azores – Colección Atlántica</h3>
                         </div>
                     </NavLink>   
@@ -48,10 +81,7 @@ const Desarrollos = () => {
                         <div className="flex flex-col gap-4">
                             <img    src="Madeira.jpeg" className="w-full h-56 object-cover rounded-3xl transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl" 
                                     alt='Invierte en rosenda' />
-                            <p className="text-gray-500 font-chivo"> 
-                                Refugio entre lo verde y lo sofisticado.
-                                        Inspirada en los paisajes exuberantes de Portugal, Madeira ofrece un estilo de vida íntimo, rodeado de vegetación, diseño sobrio y materiales nobles. Un enclave para reconectar con lo esencial sin renunciar a lo extraordinario. Mensualidades: $3,400.00 MXN.
-                                 </p>
+                         
                                 <h3 className="text-2xl font-bold text-gray-400 ">Madeira - Colección Natural </h3> 
                         </div>
                     </NavLink>   
@@ -60,26 +90,36 @@ const Desarrollos = () => {
                         <div className="flex flex-col gap-4">
                             <img src="Madeira.jpeg" className="w-full h-56 object-cover rounded-3xl transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl" 
                                 alt ='Plusvalía en Mérida'/>
-                            <p className="text-gray-500 font-chivo">Sofisticación cálida, esencia insular.
-Boracay captura la frescura del trópico con una visión contemporánea. Ambientes amplios, luz natural y acabados cálidos crean un oasis de relajación privada, ideal para quienes encuentran el lujo en la sencillez bien pensada. Mensualidades $3,124.00 MXN</p>
-                                <h3 className="text-2xl font-bold text-gray-400 ">Boracay – Colección Tropical </h3>
-                        </div>
-                    </NavLink>   
-
-                    <NavLink className ="text-center " to = "/indo" > 
-                        <div className="flex flex-col gap-4">
-                                <img src="Isla_2.webp" className="w-full h-56 object-cover rounded-3xl transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl"
-                                    alt='Yucatan crecimiento económico'/>
-                                <p className="text-gray-500 font-chivo"> 
-                                Yucatán es considerado como el estado con mayor crecimiento económico de México, líder en la generación de energía eléctrica. </p>
-                                    <h3 className="text-xl font-bold text-orange-900 line-through">INDO Parque Industrial</h3>
-                                    <h3 className="text-2xl font-bold text-gray-400 ">SOLD OUT</h3>
+                              <h3 className="text-2xl font-bold text-gray-400 ">Boracay – Colección Tropical </h3>
                         </div>
                     </NavLink>   
 
                 </div>
             </div>
+
+            <section className="bg-white dark:bg-gray-900 dark:text-white px-6 py-16">
+
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+                    {desarrollos.map((d, idx) => (
+                    <NavLink to={d.to} key={idx} className="group">
+                        <div className="bg-gray-50 dark:bg-gray-800 rounded-3xl shadow-xl hover:shadow-2xl transition overflow-hidden">
+                        <img
+                            src={d.imagen}
+                            alt={d.nombre}
+                            className="w-full h-64 object-cover group-hover:scale-105 transition-transform duration-500"
+                        />
+                        <div className="p-6 space-y-4 text-left">
+                            <h3 className="text-2xl font-bold text-diamante dark:text-primary_isla">{d.nombre}</h3>
+                            <p className="text-gray-700 dark:text-gray-300 font-chivo text-base leading-relaxed">{d.descripcion}</p>
+                        </div>
+                        </div>
+                    </NavLink>
+                    ))}
+                </div>
+            </section>
         </div>
+
+        
     )
 }
 
