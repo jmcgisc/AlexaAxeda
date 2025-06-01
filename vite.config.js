fe544131665eece039417d8f37bdf3e7675f53f8
Jose Carreiro <jmcgisc@gmail.com>
2025-05-31 22:39:26 -0600

    Fix: aseguro que @vitejs/plugin-react esté disponible en Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  
-import Markdown from '@pity/vite-plugin-react-markdown'
-import viteCompression from 'vite-plugin-compression';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression(),
-    Markdown({
-      // wrapperComponentName: 'ReactMarkdown',
-      // wrapperComponentPath: './src/components/page',
-      // if you want use components in md file, please add it in this
-      // [ComponentName]: `componentPath`
-      // // ?‍?: the `ComponentName` must be `CamelCase`
-      // importComponentsPath: {
-      //   ReactTest: './src/components/pages/mdtest',
-      // },
-      // markdownItUses: [
-      //   prism,
-      // ],
-    }),
-  ], 
-})
+  plugins: [react()],
+});
