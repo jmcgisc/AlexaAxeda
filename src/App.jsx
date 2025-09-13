35082b46c244c38c0bd18706d2e16215db07cfba
Jose Carreiro <jmcgisc@gmail.com>
2025-09-13 11:24:28 -0600

    feature:agentes ia

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function App() {
         {showExtras && (
                 <>
                   <ChatBubble />
-                  <ChatbotGPT />
+                  {/* <ChatbotGPT /> */}
                   <CustomWhatsApp />
                 </>
               )}   
